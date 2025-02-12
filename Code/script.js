4c9bbd7d19b3a37cc48622ecc8b12603e8a4aaa7
Adarsh-067 <adarshchaubey711@gmail.com>
2025-02-12 09:24:11 +0530

    Upload portfolio project in github

---
+++ Code/script.js
@@ -0,0 +1,15 @@
+var input = document.querySelector("#inp").Value;
+let btn1 = document.querySelector("#btn1");
+let para = document.querySelector("#para");
+
+btn1.addEventListener("click",()=>{
+  para.innerHTML = input;
+});
+
+
+
+
+
+
+
+ 
